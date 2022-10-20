3e200550596676c3fa9c20cc7dc819b83a80fc86
Т <4@4.com>
2022-10-20 19:02:50 +0600

    lab 1

---
+++ app/src/main/java/com/example/lab1/HeroesData.kt
@@ -0,0 +1,41 @@
+package com.example.lab1
+
+
+object HeroesData {
+    val heros = listOf(
+        Hero(
+            R.drawable.doctor_strange,
+            "Doctor strange",
+            ""
+
+        ),
+        Hero(
+            R.drawable.deadpool,
+            "Deadpool",
+            ""
+
+        ),
+        Hero(
+            R.drawable.spider_man,
+            "Spider man",
+            ""
+
+            ),
+
+        Hero(
+            R.drawable.iron_man,
+            "Iron man",
+            ""
+
+            ),
+
+
+        Hero(
+            R.drawable.thor,
+            "Thor",
+            ""
+
+            ),
+    )
+
+}
